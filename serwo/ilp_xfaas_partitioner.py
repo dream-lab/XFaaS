993e29b63d0282fe326bde4b9419d3fbb8cd09a9
Varad Vinod Kulkarni <varad.kulkarni@Varads-MacBook-Air-2.local>
2023-06-27 00:39:46 +0530

    ilp end to end working

--- serwo/ilp_xfaas_partitioner.py
+++ serwo/ilp_xfaas_partitioner.py
@@ -26,9 +26,19 @@ def get_optimal_partitions(latencies_benchmark, data_transfers_benchmark,data_tr
 
     mdl.minimize(mdl.sum(P[m,m+1,i,j]*data_transfers_benchmark[i][j] for m in range(0,num_parts-1) for i in range(0,num_csps) for j in range(0,num_csps))+mdl.sum(P[m,m+1,i,j]*latencies_benchmark[i][m] for m in range(0,num_parts-1) for i in range(0,num_csps) for j in range(0,num_csps))+mdl.sum(P[num_parts-2,num_parts-1,i,j]*latencies_benchmark[j][num_parts-1] for i in range(0,num_csps) for j in range(0,num_csps)))
 
-    solution = mdl.solve(log_output=True)
-    print(solution)
-    print(mdl.lp_string)
+    solution = mdl.solve()
+    latency = solution.objective_value
+
+    data = [v.name.split('_') + [solution.get_value(v)] for v in mdl.iter_variables()]
+
+    cloud_indices = []
+    for d in data:
+        if d[-1] == 1:
+            cloud_indices.append(int(d[-3]))
+            cc = d[-2]
+    cloud_indices.append(int(cc))
+    return cloud_indices,latency
+
 
 
 latencies_benchmark = [[33, 4139, 3095, 14963.68, 3095, 33], [10, 514, 1473, 13816, 1473, 10]]
@@ -41,4 +51,4 @@ is_fan_in = [False, False, False, True, False, False]
 # data_transfers = [64,64,64]
 # is_fan_in = [False,False,False,False]
 
-get_optimal_partitions(latencies_benchmark,data_transfer_benchmark,data_transfers,is_fan_in)
+print(get_optimal_partitions(latencies_benchmark,data_transfer_benchmark,data_transfers,is_fan_in))
