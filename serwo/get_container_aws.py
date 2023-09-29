c523b7f44647fab2e1374c880a2a5d908a22b0aa
NikhilReddy-2 <nikhilreddy156@gmail.com>
2023-09-29 16:06:46 +0530

    Added func call to get container count

--- serwo/get_container_aws.py
+++ serwo/get_container_aws.py
@@ -60,67 +60,69 @@ def get_max_concurrent_execs(func_name):
 
     return timestamps, concurrent_execs
 
-# Initialize the Lambda client
-lambda_client = boto3.client('lambda', region_name='ap-south-1')
 
-# Replace 'YourLayerName-' with the actual name of your Lambda application (Layer)
-wf_name = sys.argv[1]
-application_name_prefix = wf_name
+def get_container_count():
+    # Initialize the Lambda client
+    lambda_client = boto3.client('lambda', region_name='ap-south-1')
 
-# Initialize an empty list to store all functions
-all_functions = []
+    # Replace 'YourLayerName-' with the actual name of your Lambda application (Layer)
+    wf_name = sys.argv[1]
+    application_name_prefix = wf_name
 
-# Initial call to list_functions
-response = lambda_client.list_functions()
+    # Initialize an empty list to store all functions
+    all_functions = []
 
-# Filter and add the functions with the correct prefix
-all_functions.extend([func for func in response['Functions'] if func['FunctionName'].startswith(application_name_prefix)])
+    # Initial call to list_functions
+    response = lambda_client.list_functions()
 
-# Check if there are more functions to retrieve
-while 'NextMarker' in response:
-    next_marker = response['NextMarker']
-    response = lambda_client.list_functions(Marker=next_marker)
+    # Filter and add the functions with the correct prefix
     all_functions.extend([func for func in response['Functions'] if func['FunctionName'].startswith(application_name_prefix)])
 
-# Sort the functions by their names
-all_functions.sort(key=lambda x: x['FunctionName'])
-concurrent_executions = {}
+    # Check if there are more functions to retrieve
+    while 'NextMarker' in response:
+        next_marker = response['NextMarker']
+        response = lambda_client.list_functions(Marker=next_marker)
+        all_functions.extend([func for func in response['Functions'] if func['FunctionName'].startswith(application_name_prefix)])
 
-# Print or process all_functions as needed
-timestamps = []
+    # Sort the functions by their names
+    all_functions.sort(key=lambda x: x['FunctionName'])
+    concurrent_executions = {}
 
-# Initialize variables to track the minimum and maximum timestamps
-min_timestamp = None  # Initialize to positive infinity
-max_timestamp = None  # Initialize to negative infinity
+    # Initialize variables to track the minimum and maximum timestamps
+    min_timestamp = None  # Initialize to positive infinity
+    max_timestamp = None  # Initialize to negative infinity
 
-concurr = []
-for function in all_functions:
-    t, c = get_max_concurrent_execs(function['FunctionName'])
-    for i in range(len(t)):
-        timestamp = t[i]
-        concurrent = c[i]
+    for function in all_functions:
+        t, c = get_max_concurrent_execs(function['FunctionName'])
+        for i in range(len(t)):
+            timestamp = t[i]
+            concurrent = c[i]
 
-        if min_timestamp is None or timestamp < min_timestamp:
-            min_timestamp = timestamp
-        if max_timestamp is None or timestamp > max_timestamp:
-            max_timestamp = timestamp
+            if min_timestamp is None or timestamp < min_timestamp:
+                min_timestamp = timestamp
+            if max_timestamp is None or timestamp > max_timestamp:
+                max_timestamp = timestamp
 
-        timestamp = timestamp.astimezone(ist_timezone)
-        timestamp = timestamp.strftime('%s')
+            timestamp = timestamp.astimezone(ist_timezone)
+            timestamp = timestamp.strftime('%s')
 
-        if timestamp in concurrent_executions:
-            concurrent_executions[timestamp] += concurrent
-        else:
-            concurrent_executions[timestamp] = concurrent
+            if timestamp in concurrent_executions:
+                concurrent_executions[timestamp] += concurrent
+            else:
+                concurrent_executions[timestamp] = concurrent
 
-# timestamps.sort()
-print(concurrent_executions)
-min_timestamp = min_timestamp.astimezone(ist_timezone)
+    # timestamps.sort()
+    print(concurrent_executions)
+    min_timestamp = min_timestamp.astimezone(ist_timezone)
 
-op_file = f"aws_ap_south_1_{wf_name}_{min_timestamp.strftime('%d-%m-%Y-%H-%M-%S')}.txt"
+    op_file = f"aws_ap_south_1_{wf_name}_{min_timestamp.strftime('%d-%m-%Y-%H-%M-%S')}.txt"
 
-with open(op_file, 'w', newline='') as ofile:
-    csvwriter = csv.writer(ofile)
+    with open(op_file, 'w', newline='') as ofile:
+        csvwriter = csv.writer(ofile)
 
-    for ti, val in concurrent_executions.items():
-        csvwriter.writerow([ti, val])
+        for ti, val in concurrent_executions.items():
+            csvwriter.writerow([ti, val])
+
+    return concurrent_executions
+
+get_container_count()
