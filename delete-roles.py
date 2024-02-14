b7d252b6bb6df9d3fcfe60ed28a8b83df2e26c4f
Varad Vinod Kulkarni <varad.kulkarni@Varads-MacBook-Air-2.local>
2024-02-14 23:46:06 +0530

    git cleanup

--- delete-roles.py
+++
@@ -1,7 +0,0 @@
-import os
-import json
-
-if __name__ == "__main__":
-    with open("all-roles.json", "r") as file:
-        contents = json.load(file)
-        roles = contents["Roles"]
--- run_plot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# verify all parameters are correct before executing
-python3 xfaas_benchmarksuite_plotgen_vk.py \
---user-dir /Users/varad.kulkarni/xfaas/XFaaS/serwo/examples/graphAws \
---artifacts-file aws-static-small-1rps.json \
---interleaved True \
---format pdf \
---out-dir aws-static-1-rps-small \
