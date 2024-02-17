dfcbd37af91f766b134fb161a49f7a2bedb3b9dd
Varad Vinod Kulkarni <varad.kulkarni@Varads-MacBook-Air-2.local>
2024-02-17 13:03:46 +0530

    updated shell and plotting scripts

--- sanity_check.sh
+++ sanity_check.sh
@@ -13,5 +13,19 @@ python3 serwo/xfaas_run_benchmark.py \
 --teardown-flag 0 \
 --client-key localhost
 
+sleep10
+
+python3 serwo/xfaas_run_benchmark.py \
+--csp azure \
+--region eastus \
+--max-rps 1 \
+--duration 10 \
+--payload-size small \
+--dynamism static \
+--wf-name graph \
+--wf-user-directory /XFBench/workflows/custom_workflows/graph_processing_wf \
+--dag-file-name dag.json \
+--teardown-flag 0 \
+--client-key localhost
 
 echo "Sanity check completed successfully!"
