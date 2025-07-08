5862137da973dcf2024750422a8b70c6c27a7c2b
Varad Vinod Kulkarni <varad.kulkarni@Varads-MacBook-Air-2.local>
2025-07-08 11:59:57 +0530

    minor fix

--- setup-xfaas.sh
+++ setup-xfaas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export AZURE_SUBSCRIPTION_ID=9dc7b503-5f49-4742-8f12-07fad514c633
-export XFAAS_DIR=/Users/varad.kulkarni/xfaas/XFaaS
-export XFAAS_WF_DIR=/Users/varad.kulkarni/xfaas/xfaas-workloads
-export XFBENCH_DIR=/Users/varad.kulkarni/xfaas/xfaas-workloads
+export AZURE_SUBSCRIPTION_ID=<>
+export XFAAS_DIR=<>
+export XFAAS_WF_DIR=<>
+export XFBENCH_DIR=<>
